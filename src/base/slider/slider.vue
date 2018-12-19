ec82972dd8e89b2ec8c508b4ebc27d8d7310d21c
wudiweigit <18818235606@163.com>
2018-12-19 17:06:36 +0800

    slider优化

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -18,7 +18,7 @@
 </template>
 
 
-<script >
+<!-- <script >
     import {addClass} from 'common/js/dom'
     import BScroll from 'better-scroll'
   
@@ -126,7 +126,134 @@
        
       }
     }
-  </script>
+  </script> -->
+
+
+
+
+<script >
+  import {addClass} from 'common/js/dom'
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: 'slider',
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
+    data() {
+      return {
+        dots: [], 
+          currentPageIndex: 0 
+        }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()  // [  2-1.17 ]  一开始这里不用传表示 underfined 
+
+        this._initDots()
+        this._initSlider()
+
+        if(this.autoPlay){
+          this._play()  
+        }
+
+      }, 20)
+      // [  2-1.16  ]当窗口发生改变时轮播图会有问题
+      window.addEventListener('resize', () => {
+        if(!this.slider){  // 如果this.slider还没有初始化时什么都不做
+          return 
+        }
+        // this._setSliderWidth()  //否则重新调用方法计算宽度==》会存在问题： 因为每次调用该方法时都会重新给width赋予两倍的宽度（width += 2 * sliderWidth）
+        this._setSliderWidth(true)  //[  2-1.16-1  ] 解决： 为其设置标识位（isResize ==>标识这个是不是resize过来的）
+        this.slider.refresh()   // [  2-1.18 ]BScroll 提供的借口当宽度发生变化时调用该方法
+      })
+
+    },
+    
+    methods: {
+      _setSliderWidth(isResize) {//[  2-1.16-1  ] [  2-1.17 ]  !underfined 返回的是true
+        this.children = this.$refs.sliderGroup.children
+        console.log(this.children) 
+
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop && !isResize) {//[  2-1.16-1  ]
+          width += 2 * sliderWidth
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,  
+          snap: true,  
+          snapLoop: this.loop,   
+          snapThreshold: 0.3, 
+          snapSpeed: 400 ,
+          // click: true,  // [   2-1.19   ] 在手机中点击图片时不会进行跳转是因为BScroll与fastclick产生冲突这里要把其注释掉 
+        })
+
+
+        this.slider.on( 'scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX 
+          if(this.loop){ 
+            pageIndex -=1
+          }
+          this.currentPageIndex = pageIndex
+
+
+          if(this.autoPlay){
+            clearTimeout(this.timer)
+            this._play()
+
+          }
+
+
+        } )
+
+      },
+      _initDots() { 
+        this.dots = new Array(this.children.length) 
+      },
+      _play(){
+        let pageIndex = this.currentPageIndex + 1
+        if(this.loop){
+          pageIndex += 1
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400) 
+        }, this.interval )
+      }
+
+      
+      
+    },
+    destroyed() { // [2-1.21  ]当组件中有计时器资源时将组件销毁时删除
+      clearTimeout(this.timer)
+    },
+  }
+</script>
+
+
 
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
